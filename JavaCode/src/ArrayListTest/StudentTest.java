f039c6d7e5af685aee2ef6f59e665615ad1bf718
NewPark <NewPark@DESKTOP-7CRB3P7>
2019-09-26 11:54:06 +0900

    ArrayList Use

--- JavaCode/src/ArrayListTest/StudentTest.java
+++ JavaCode/src/ArrayListTest/StudentTest.java
@@ -3,8 +3,20 @@ package ArrayListTest;
 public class StudentTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Student studentLee = new Student(1001,"lee");
+		studentLee.addSubject("국어", 100);
+		studentLee.addSubject("영어", 90);
+		studentLee.addSubject("사회", 80);
+		
+		Student studentKim = new Student(1002,"Kim");
+		studentKim.addSubject("국어", 100);
+		studentKim.addSubject("영어", 80);
+		studentKim.addSubject("수학", 70);
+		
+		
+		studentLee.showStudentInfo();
+		studentKim.showStudentInfo();
 	}
 
 }
--- JavaCode/src/ArrayListTest/Subject.java
+++ JavaCode/src/ArrayListTest/Subject.java
@@ -1,5 +1,24 @@
 package ArrayListTest;
 
 public class Subject {
-	private int studentID;
+	
+	public Subject(String name,int score) {
+		this.name=name;
+		this.score=score;
+		
+	}
+	private int score;
+	private String name;
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
